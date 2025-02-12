fbdc9290c445ec4ab638da7a44a3e55e455af038
Elijah L <Elijah2.Love@live.uwe.ac.uk>
2025-02-12 20:52:33 +0000

    added entity chasing player

--- Assets/Scripts/Entities/EntityChase.cs
+++ Assets/Scripts/Entities/EntityChase.cs
@@ -2,17 +2,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.AI;
 
 public class EntityChase : MonoBehaviour
 {
+    Transform player;
+
+    NavMeshAgent nav_mesh_agent;
+
     void Start()
     {
-        
+        player = GameObject.FindWithTag("Player").transform;
+        nav_mesh_agent = GetComponent<NavMeshAgent>();
     }
 
     void Update()
     {
-        
+        nav_mesh_agent.destination = player.position;
     }
 
     void OnTriggerEnter(Collider other)
