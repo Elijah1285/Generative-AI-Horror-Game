5fe83a153b265b1fad705b92c3d5e748b0e32872
Elijah L <Elijah2.Love@live.uwe.ac.uk>
2025-02-12 21:36:35 +0000

    added activator trigger for entity

---
+++ Assets/Scripts/Misc/ActivatorTrigger.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActivatorTrigger : MonoBehaviour
+{
+    [SerializeField] GameObject object_to_activate;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            object_to_activate.SetActive(true);
+        }
+    }
+}
