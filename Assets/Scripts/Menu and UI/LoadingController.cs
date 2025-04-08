1f0695a13bc569765c01b6555989f27c2df2e491
Elijah L <Elijah2.Love@live.uwe.ac.uk>
2025-04-08 11:07:32 +0100

    added loading bar

--- Assets/Scripts/Menu and UI/LoadingController.cs
+++ Assets/Scripts/Menu and UI/LoadingController.cs
@@ -2,11 +2,27 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class LoadingController : MonoBehaviour
 {
+    [SerializeField] Slider loading_bar;
+
     void Start()
     {
-        SceneManager.LoadScene("Level " + PlayerPrefs.GetInt("current_level").ToString());
+        StartCoroutine(loadLevelAsync());
+        //SceneManager.LoadScene("Level " + PlayerPrefs.GetInt("current_level").ToString());
+    }
+
+    IEnumerator loadLevelAsync()
+    {
+        AsyncOperation load_operation = SceneManager.LoadSceneAsync("Level " + PlayerPrefs.GetInt("current_level").ToString());
+
+        while (!load_operation.isDone)
+        {
+            float progress_value = Mathf.Clamp01(load_operation.progress / 0.9f);
+            loading_bar.value = progress_value;
+            yield return null;
+        }
     }
 }
